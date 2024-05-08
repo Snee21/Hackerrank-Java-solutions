f024f8d97559c3c0f0d341dba54002f49c62e71f
Sneka Sivananthaperumal <132367543+Snee21@users.noreply.github.com>
2024-05-08 20:40:29 +0530

    Add files via upload

---
+++ Java stdin and stdout I.java
@@ -0,0 +1,12 @@
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sc.close();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
